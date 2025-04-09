11c734709fee48f057d621798a68cd7b8eb97d25
Yiannis Ntakoumis <giannisdak@gmail.com>
2025-04-09 16:22:35 +0300

    Initialize project

---
+++ logger/logger.js
@@ -0,0 +1,71 @@
+// First Example
+//const winston = require('winston')
+// const logger = winston.createLogger({
+//   format: winston.format.json(),
+//   transports: [new winston.transports.Console()],
+// })
+
+// Second Example
+
+// const { format, createLogger, transports } = require('winston') // calling the libraries from winston
+// const { combine, timestamp, label, printf } = format // calling the methods
+// const CATEGORY = 'Products app logs'
+
+// const customFormat = printf(({ level, message, label, timestamp }) => {
+//   // i want a string with date, label and message
+//   return `${timestamp} [${label}: ${level}, ${message}]`
+// })
+
+// const logger = createLogger({
+//   // level: "warn",
+//   format: combine(label({ label: CATEGORY }), timestamp(), customFormat), //
+//   transports: [new transports.Console()],
+// })
+
+// Third Example
+require('dotenv').config()
+
+require('winston-daily-rotate-file')
+require('winston-mongodb')
+const { format, createLogger, transports } = require('winston') // calling the libraries from winston
+const { combine, timestamp, label, printf, prettyPrint } = format // calling the methods
+const CATEGORY = 'Products app logs'
+const fileRotateTransport = new transports.DailyRotateFile({
+  filename: './logs/rotate-%DATE%.log',
+  datePattern: 'DD-MM-YYYY',
+  maxFiles: '7d',
+  level: 'error',
+})
+
+const logger = createLogger({
+  format: combine(
+    label({ label: 'MY LABEL PRODUCTS APP' }),
+    timestamp({ format: 'DD-MM-YYYY HH:mm:sss' }),
+    // format.json()
+    prettyPrint()
+  ),
+  transports: [
+    new transports.Console(),
+    fileRotateTransport,
+    new transports.File({
+      filename: 'logs/example.log',
+    }),
+    new transports.File({
+      level: 'warn',
+      filename: 'logs/warn.log',
+    }),
+
+    new transports.File({
+      level: 'info',
+      filename: 'logs/info.log',
+    }),
+
+    new transports.MongoDB({
+      level: 'warn',
+      db: process.env.MONGODB_URI,
+      collection: 'server_logs',
+      format: format.combine(format.timestamp(), format.json()),
+    }),
+  ],
+})
+module.exports = logger
